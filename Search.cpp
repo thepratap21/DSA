a4e4c1b79d645bcd4758e24ea9819e869ba07ccb
Abhay Pratap Singh <thepratapabhay21@gmail.com>
2026-09-01 16:25:48 +0530

    Add binary search implementation

---
+++ Search.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+using namespace std;
+
+int binarySearch(int arr[], int n, int target) {
+    int low = 0;
+    int high = n - 1;
+
+    while (low <= high) {
+        int mid = low + (high - low) / 2;
+
+        if (arr[mid] == target) {
+            return mid;
+        }
+        else if (arr[mid] < target) {
+            low = mid + 1;
+        }
+        else {
+            high = mid - 1;
+        }
+    }
+
+    return -1;
+}
+
+int main() {
+    int arr[] = {2, 5, 8, 12, 16, 23, 38, 56, 72};
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    int target;
+    cout << "Enter element to search: ";
+    cin >> target;
+
+    int result = binarySearch(arr, n, target);
+
+    if (result != -1)
+        cout << "Element found at index: " << result << endl;
+    else
+        cout << "Element not found" << endl;
+
+    return 0;
+}
