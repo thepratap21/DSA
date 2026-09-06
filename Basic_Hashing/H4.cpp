a2563579bfeb948701e6fde9303df4e269ecc0ec
Abhay Pratap Singh <thepratapabhay21@gmail.com>
2026-09-06 12:22:17 +0530

    Element with maximum frequency in array

---
+++ Basic_Hashing/H4.cpp
@@ -0,0 +1,38 @@
+#include<bits/stdc++.h>
+using namespace std;
+int  main()
+{
+    cout<<"Enter the no of arrays";
+    int n;
+    cin>>n;
+    int arr[n];
+    cout<<"Enter the array elements";
+    for(int i=0;i<n;i++)
+    {
+        cin>>arr[i];
+
+    }
+    int hash[13]={0};
+    for(int i=0;i<n;i++)
+    {
+        hash[arr[i]]+=1;
+    }
+    int q;
+    
+   /* cin>>q;
+    while(q--){
+        int number;
+        cin>>number;
+        cout<<"The number"<<number<<"appears"<<hash[number]<<"times"<<endl;
+    }
+    */
+   int max=hash[0];
+for(int i=0;i<n;i++)
+{
+    if(hash[i+1]>max)
+    max=i+1;
+    else
+    i++;
+}
+cout<<"Element with max frequency is" <<" "<<max<<endl;
+}
