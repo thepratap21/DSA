2767db46d5977f6b501a6345d3523909d8ebb87b
Abhay Pratap Singh <thepratapabhay21@gmail.com>
2026-09-12 13:24:35 +0530

    Optimal approach for second largest

---
+++ Array/Easy/SLargest3.cpp
@@ -0,0 +1,38 @@
+#include<bits/stdc++.h>
+using namespace std;
+int Sndlargest(int arr[],int n)
+{
+     int lar=arr[0];
+  int sl=-1;
+    for(int i=0;i<n-1;i++)
+    {
+        if(arr[i+1]>arr[i])
+        {
+         lar=arr[i+1];
+         sl=arr[i];
+        }
+    }
+
+cout<<"Second Largest element is"<<" "<<sl;
+    return 0;
+}
+
+int main()
+{
+    int n;
+    cout<<"Enter the size of array";
+    cin>>n;
+    int arr[n];
+    cout<<"Enter array elements";
+    for(auto &it : arr)
+    {
+        cin>>it;
+    }
+    cout<<"The entered elements are"<<endl;
+    for(auto &it : arr)
+    {
+        cout<<it<<" ";
+    }
+Sndlargest(arr,n);
+return 0;
+}
