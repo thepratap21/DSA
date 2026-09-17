b6e9ae9fd886d8f6f4c269ff52539f41e0bbf2a4
Abhay Pratap Singh <thepratapabhay21@gmail.com>
2026-09-17 23:31:09 +0530

    Upper Bound

---
+++ BinarySearch/upperbound.cpp
@@ -0,0 +1,31 @@
+#include<bits/stdc++.h>
+using namespace std;
+int search(int arr[],int n)
+{   int k;
+    cout<<"Enter the element whose upper bound you need to search"<<" ";
+    cin>>k;
+    int pos=0;
+    for(int i=1;i<=n;i++)
+    {
+        if(arr[i]<=k)
+        {
+            pos++;
+        }
+
+    }
+    cout<<"The index of upper bound is"<<" "<<pos+1;
+    
+    return 0;
+}
+int main()
+{
+    int n;
+    cout<<"Enter the size of array";
+    cin>>n;
+    int arr[n];
+    cout<<"Enter array elements"<<" "<<endl;
+    for(int i=1;i<=n;i++)
+    cin>>arr[i];
+    cout<<endl;
+    search(arr,n);
+}
