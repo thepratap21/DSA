e8bfd269878ea85bd43383b2b947086477ac4010
Abhay Pratap Singh <thepratapabhay21@gmail.com>
2026-09-22 20:48:24 +0530

    LCM using GCD method

---
+++ Basics/LCM.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int a,b,gcd=0;
+    cout<<"Enter two numbers to find LCM"<<" ";
+    cin>>a>>b;
+    for(int i=1;i<=a;i++)
+    {
+        if(a%i==0 && b%i==0)
+        {
+          gcd=i;  
+        }
+    }
+
+    cout<< " LCM of Two Numbers is"<< " "<<(a*b)/gcd;
+}
